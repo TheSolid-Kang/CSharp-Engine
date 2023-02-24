efc3489fa5a044d5e2013e01a5255eecfbb175e4
TheSolid-KANG <tortoise8947@gmail.com>
2023-02-24 13:00:16 +0900

    FTPMgr 사용하지 않는 함수 삭제

--- 03.CFTPMgr/CFTPMgr.cs
+++ 03.CFTPMgr/CFTPMgr.cs
@@ -8,56 +8,44 @@ using System.Windows.Forms;
 
 namespace Engine._03.CFTPMgr
 {
-  public enum FUNC : int { ONE }
-  public delegate object del_func(params object[] _arr_obj_sender);
-
-  public class FTP_INFO
+  public class CFtpInfo
   {
-    public FTP_INFO()
+    public CFtpInfo()
     {
-      this.ip_addr = "127.0.0.1"; //IP 주소
-      this.port = "21"; //FTP 접속 Port
-      this.user = "접속계정"; //FTP 접속 계정
-      this.pwd = "비밀번호"; //FTP 계정 비밀번호
+      this._ipAddress = "127.0.0.1"; //IP 주소
+      this._port = "21"; //FTP 접속 _port
+      this._user = "접속계정"; //FTP 접속 계정
+      this._password = "비밀번호"; //FTP 계정 비밀번호
       this.server_path = @"www/data/";
     }
-    public FTP_INFO(string ip_addr, string port, string user, string pwd)
+    public CFtpInfo(string _ipAddress, string _port, string _user, string _password)
     {
-      this.ip_addr = ip_addr;
-      this.port = port;
-      this.user = user;
-      this.pwd = pwd;
+      this._ipAddress = _ipAddress;
+      this._port = _port;
+      this._user = _user;
+      this._password = _password;
       server_path = @"www/data/";
     }
 
-    public string ip_addr { get; set; }
-    public string user { get; set; }
-    public string pwd { get; set; }
-    public string port { get; set; }
-
-    public string connect_result { get; set; }
+    public string _ipAddress { get; set; }
+    public string _user { get; set; }
+    public string _password { get; set; }
+    public string _port { get; set; }
 
     public string server_path { get; set; }
-    public string local_path { get; set; }
-
   }
   public class CFTPMgr : IDisposable
   {
-    #region DEFAULT (생성자/ 홀더/ INSTANCE / Destroy)       
+    #region DEFAULT (생성자/ 홀더/ Instance)
     private CFTPMgr() { }
-    private static readonly Lazy<CFTPMgr> _p_instance = new Lazy<CFTPMgr>(() => new CFTPMgr());
-    public static CFTPMgr get_instance() => _p_instance.Value;
-    public void DestroyMgr() => GC.Collect();
+    private static readonly Lazy<CFTPMgr> _instance = new Lazy<CFTPMgr>(() => new CFTPMgr());
+    public static CFTPMgr GetInstance() => _instance.Value;
     #endregion
 
 
     #region 제어용 멤버변수
-    public Dictionary<FUNC, del_func> m_map_func = new Dictionary<FUNC, del_func>();
     public bool isOn = true;
-
     #endregion
-
-
 
     #region FTP 멤버변수
     public delegate void ExceptionEventHandler(string LocationID, Exception ex);
@@ -65,65 +53,54 @@ namespace Engine._03.CFTPMgr
     public Exception LastException = null;
 
     public bool IsConnected { get; set; }
-    private FTP_INFO _ftp_info = new FTP_INFO();
-    public FTP_INFO ftp_info => _ftp_info;
+    private CFtpInfo _ftpInfo = new CFtpInfo();
     #endregion
 
 
     #region 멤버함수 선언부
     //1. 서버 연결
-    public bool ConnectToServer() => connect_to_server();
-    public bool ConnectToServer(string ip, string port, string userId, string pwd) => connect_to_server(ip, port, userId, pwd);
+    public bool ConnectToServer() => _ConnectToServer();
+    public bool ConnectToServer(string ip, string _port, string _userId, string _password) => _ConnectToServer(ip, _port, _userId, _password);
 
     //2. 서버에 파일 업로드 
-    public bool UpLoad(string _server_path, string _local_file_path) => upload(_server_path, _local_file_path);
-    //2_2. 서버에 여러 파일 업로드 
-    public bool UpLoad(string _server_path, List<string> _local_file_path) => upload(_server_path, _local_file_path);
-    //2_3. 서버에 여러 파일 업로드
-    public bool UpLoad(List<string> _list_server_file_path, string _local_file_path) => upload(_list_server_file_path, _local_file_path);
-    public void MakeDirectory(string _server_path) => make_directory(_server_path);
+    public bool UpLoad(string _serverDirPath, string _localFilePath) => _UpLoad(_serverDirPath, _localFilePath);
+    public void MakeDirectory(string _serverDirPath) => _MakeDirectory(_serverDirPath);
 
     //3. 서버에서 파일 다운로드
-    public bool DownLoad(string _local_file_path, string _server_path) => download(_local_file_path, _server_path);
-    //3_2. 서버에서 여러 파일 다운로드
-    public bool DownLoad(string _local_file_path, List<string> _list_server_file_path) => download(_local_file_path, _list_server_file_path);
-    public void CheckDirectory(string _local_file_path) => check_directory(_local_file_path);
+    public bool DownLoad(string _localFilePath, string _serverDirPath) => _DownLoad(_localFilePath, _serverDirPath);
+    public void CheckDirectory(string _localFilePath) => _CheckDirectory(_localFilePath);
 
     //4. 서버의 해당 폴더에 담긴 파일 리스트 가져옴
-    public List<string> GetFileList(string _server_path) => get_file_list(_server_path);
+    public List<string> GetFileList(string _serverDirPath) => _GetFileList(_serverDirPath);
 
     //5. 파일 다이어로그 출력_여러 파일 선택 가능
     public List<string> OpenDlg() => open_dlg();
-    //6. 파일 다이어로그 출력_저장 위치 선택
-    public string GetSaveFilePath() => get_save_file_path();
 
     //7. 서버의 특정 폴더에 저장된 파일 삭제  
-    public bool DeleteSavedFileInServer(string _server_file_path) => delete_saved_file_in_server(_server_file_path);
-    //7_2. 서버에서 여러 파일 다운로드
-    public bool DeleteSavedFileInServer(List<string> _list_server_file_path) => delete_saved_file_in_server(_list_server_file_path);
-    //7_3. 서버의 특정 폴더에 저장된 파일 전체 삭제  
-    public bool DeleteAllSavedFileInServer(string _server_path) => delete_all_saved_file_in_server(_server_path);
+    public bool DeleteSavedFileInServer(string _serverFilePath) => _DeleteSavedFileInServer(_serverFilePath);
+    //7_2. 서버의 특정 폴더에 저장된 파일 전체 삭제  
+    public bool DeleteAllSavedFileInServer(string _serverDirPath) => _DeleteAllSavedFileInServer(_serverDirPath);
     #endregion
 
 
     #region 멤버함수 정의부
 
     //1. 서버 연결
-    private bool connect_to_server() => connect_to_server(_ftp_info.ip_addr, _ftp_info.port, _ftp_info.user, _ftp_info.pwd);
-    private bool connect_to_server(string ip, string port, string userId, string pwd)
+    private bool _ConnectToServer() => _ConnectToServer(_ftpInfo._ipAddress, _ftpInfo._port, _ftpInfo._user, _ftpInfo._password);
+    private bool _ConnectToServer(string ip, string _port, string _userId, string _password)
     {
       this.IsConnected = false;
-      this.ftp_info.ip_addr = ip;
-      this.ftp_info.port = port;
-      this.ftp_info.user = userId;
-      this.ftp_info.pwd = pwd;
-
-      string url = string.Format(@"FTP://{0}:{1}/", this.ftp_info.ip_addr, this.ftp_info.port);
+      this._ftpInfo._ipAddress = ip;
+      this._ftpInfo._port = _port;
+      this._ftpInfo._user = _userId;
+      this._ftpInfo._password = _password;
+      
+      string url = string.Format(@"FTP://{0}:{1}/", this._ftpInfo._ipAddress, this._ftpInfo._port);
 
       try
       {
         FtpWebRequest ftpRequest = (FtpWebRequest)WebRequest.Create(url);
-        ftpRequest.Credentials = new NetworkCredential(userId, pwd);
+        ftpRequest.Credentials = new NetworkCredential(_userId, _password);
         ftpRequest.KeepAlive = false;
         ftpRequest.Method = WebRequestMethods.Ftp.ListDirectory;
         ftpRequest.UsePassive = false;
@@ -152,29 +129,24 @@ namespace Engine._03.CFTPMgr
     }
 
     //2. 서버에 파일 업로드 
-    private bool upload(string _server_path, string _local_file_path)
+    private bool _UpLoad(string _serverDirPath, string _localFilePath)
     {
       try
       {
-        make_directory(_server_path); //폴더가 없을 수도 있으니 확인
-        FileInfo fileInf = new FileInfo(_local_file_path);
-        _server_path = _server_path.Replace('\\', '/'); //역슬래시 -> 슬래시
-        _server_path = _server_path.Replace("//", "/"); //역슬래시 -> 슬래시
-        _local_file_path = _local_file_path.Replace('\\', '/'); //역슬래시 -> 슬래시
+        _MakeDirectory(_serverDirPath); //폴더가 없을 수도 있으니 확인
+        FileInfo fileInf = new FileInfo(_localFilePath);
+        _serverDirPath = _serverDirPath.Replace('\\', '/'); //역슬래시 -> 슬래시
+        _serverDirPath = _serverDirPath.Replace("//", "/"); //역슬래시 -> 슬래시
+        _localFilePath = _localFilePath.Replace('\\', '/'); //역슬래시 -> 슬래시
 
         string url = "";
-        if (!_server_path[_server_path.Length - 1].Equals('/'))
-          url = string.Format(@"FTP://{0}:{1}/{2}", this.ftp_info.ip_addr, this.ftp_info.port, _server_path);
+        if (!_serverDirPath[_serverDirPath.Length - 1].Equals('/'))
+          url = string.Format(@"FTP://{0}:{1}/{2}", this._ftpInfo._ipAddress, this._ftpInfo._port, _serverDirPath);
         else
-          url = string.Format(@"FTP://{0}:{1}/{2}{3}", this.ftp_info.ip_addr, this.ftp_info.port, _server_path, fileInf.Name);
-        /*              
-                        long cur_date = (long)DateTime.Now.ToUniversalTime().Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
-                        Thread cur_thread = Thread.CurrentThread;
-                        var file_sys_name = cur_date.ToString() + "_" + cur_thread.ManagedThreadId + "_" + fileInf.Name;
-        */
+          url = string.Format(@"FTP://{0}:{1}/{2}{3}", this._ftpInfo._ipAddress, this._ftpInfo._port, _serverDirPath, fileInf.Name);
 
         FtpWebRequest ftpRequest = (FtpWebRequest)WebRequest.Create(url);
-        ftpRequest.Credentials = new NetworkCredential(ftp_info.user, ftp_info.pwd); //쓰기 권한이 있는 FTP 사용자 로그인 지정
+        ftpRequest.Credentials = new NetworkCredential(_ftpInfo._user, _ftpInfo._password); //쓰기 권한이 있는 FTP 사용자 로그인 지정
         ftpRequest.KeepAlive = false;
         ftpRequest.UseBinary = false;
         ftpRequest.UsePassive = false;
@@ -217,40 +189,15 @@ namespace Engine._03.CFTPMgr
       }
       return true;
     }
-    //2_2. 서버에 여러 파일 업로드
-    private bool upload(string _server_path, List<string> _list_local_file_path)
-    {
-      Action<string, string> action = (string __server_path, string _local_path) => upload(__server_path, _local_path);
-      var list_thread = new List<Thread>();
-      _list_local_file_path.ForEach(element => list_thread.Add(new Thread(() => action.Invoke(_server_path, element))));
-      list_thread.ForEach(element => element.Start());
-
-      return true;
-    }
-
-    //2_3. 서버에 여러 파일 업로드
-    private bool upload(List<string> _list_server_file_path, string _local_file_path)
-    {
-      Action<string, string> action = (string __server_path, string _local_path) => upload(__server_path, _local_path);
-      var list_thread = new List<Thread>();
-      _list_server_file_path.ForEach(element => list_thread.Add(new Thread(() => action.Invoke(element, _local_file_path))));
-      //list_thread.ForEach(element => { element.Start(); element.Join(); });
-      list_thread.ForEach(element => element.Start());
-
-      return true;
-    }
 
     //  1) 서버에 파일 업로드 할 때 사용
     //서버에 폴더가 없다면 폴더를 만든다.
-    private void make_directory(string _server_path)
+    private void _MakeDirectory(string _serverDirPath)
     {
-      if (!_server_path[_server_path.Length - 1].Equals('/'))
-        _server_path = _server_path.Remove(_server_path.LastIndexOf("/") + 1);
-      if (check_server_directory_exist(_server_path))
-        return;
+      if (!_serverDirPath[_serverDirPath.Length - 1].Equals('/'))
+        _serverDirPath = _serverDirPath.Remove(_serverDirPath.LastIndexOf("/") + 1);
 
-
-      string[] arr_directory = _server_path.Split('/');
+      string[] arr_directory = _serverDirPath.Split('/');
       string currentDir = string.Empty;
       try
       {
@@ -261,9 +208,9 @@ namespace Engine._03.CFTPMgr
             if (tmpFoler == string.Empty)
               continue;
             currentDir += "/" + tmpFoler;
-            string url = string.Format(@"FTP://{0}:{1}{2}", this.ftp_info.ip_addr, this.ftp_info.port, currentDir);
+            string url = string.Format(@"FTP://{0}:{1}{2}", this._ftpInfo._ipAddress, this._ftpInfo._port, currentDir);
             FtpWebRequest ftpRequest = (FtpWebRequest)WebRequest.Create(url);
-            ftpRequest.Credentials = new NetworkCredential(ftp_info.user, ftp_info.pwd);
+            ftpRequest.Credentials = new NetworkCredential(_ftpInfo._user, _ftpInfo._password);
             ftpRequest.Method = WebRequestMethods.Ftp.MakeDirectory;
             ftpRequest.KeepAlive = false;
             ftpRequest.UsePassive = false;
@@ -287,32 +234,27 @@ namespace Engine._03.CFTPMgr
           this.ExceptionEvent(id, ex);
       }
     }
-    private bool check_server_directory_exist(string _server_path)
-    {
 
-
-      return true;
-    }
 
     //3. 서버에서 파일 다운로드
-    private bool download(string _local_file_path, string _server_path)
+    private bool _DownLoad(string _localFilePath, string _serverDirPath)
     {
       try
       {
-        check_directory(_local_file_path);
-        _local_file_path += @"\" + _server_path.Substring(_server_path.LastIndexOf('/') + 1);
+        _CheckDirectory(_localFilePath);
+        _localFilePath += @"\" + _serverDirPath.Substring(_serverDirPath.LastIndexOf('/') + 1);
 
 
-        string url = string.Format(@"FTP://{0}:{1}/{2}", this.ftp_info.ip_addr, this.ftp_info.port, _server_path);
+        string url = string.Format(@"FTP://{0}:{1}/{2}", this._ftpInfo._ipAddress, this._ftpInfo._port, _serverDirPath);
         FtpWebRequest ftpRequest = (FtpWebRequest)WebRequest.Create(url); // "FTP://" 이기에 FtpWebRequest로 캐스팅 한 것
-        ftpRequest.Credentials = new NetworkCredential(ftp_info.user, ftp_info.pwd);
+        ftpRequest.Credentials = new NetworkCredential(_ftpInfo._user, _ftpInfo._password);
         ftpRequest.KeepAlive = false;
         ftpRequest.UseBinary = true;
         ftpRequest.UsePassive = false;
 
         using (FtpWebResponse response = (FtpWebResponse)ftpRequest.GetResponse())
         {
-          using (FileStream outputStream = new FileStream(_local_file_path, FileMode.Create, FileAccess.Write))
+          using (FileStream outputStream = new FileStream(_localFilePath, FileMode.Create, FileAccess.Write))
           {
             using (Stream ftpStream = response.GetResponseStream())
             {
@@ -342,7 +284,7 @@ namespace Engine._03.CFTPMgr
       {
         MessageBox.Show($"Download Fail! \n{ex.Message}");
         this.LastException = ex;
-        if (_server_path.Contains(@"\ZOOM\") == true)
+        if (_serverDirPath.Contains(@"\ZOOM\") == true)
           return false;
         System.Reflection.MemberInfo info = System.Reflection.MethodInfo.GetCurrentMethod();
         string id = string.Format("{0}.{1}", info.ReflectedType.Name, info.Name);
@@ -351,21 +293,11 @@ namespace Engine._03.CFTPMgr
         return false;
       }
     }
-    //3_2. 서버에서 여러 파일 다운로드
-    private bool download(string _local_file_path, List<string> _list_server_file_path)
-    {
-      Action<string, string> action = (string __server_path, string _local_path) => download(__server_path, _local_path);
-      var list_thread = new List<Thread>();
-      _list_server_file_path.ForEach(element => list_thread.Add(new Thread(() => action(_local_file_path, element))));
-      list_thread.ForEach(element => element.Start());
-
-      return true;
-    }
     //  1) 서버에서 파일 다운로드 할 때 사용
     //로컬에 폴더가 없다면 폴더를 만든다.
-    private void check_directory(string _local_file_path)
+    private void _CheckDirectory(string _localFilePath)
     {
-      FileInfo fInfo = new FileInfo(_local_file_path);
+      FileInfo fInfo = new FileInfo(_localFilePath);
       if (!fInfo.Exists)
       {
         DirectoryInfo dInfo = new DirectoryInfo(fInfo.DirectoryName);
@@ -375,15 +307,15 @@ namespace Engine._03.CFTPMgr
     }
 
     //4. 해당 폴더에 담긴 파일 리스트 가져옴
-    private List<string> get_file_list(string _server_path)
+    private List<string> _GetFileList(string _serverDirPath)
     {
       List<string> resultList = new List<string>();
       StringBuilder result = new StringBuilder();
       try
       {
-        string url = string.Format(@"FTP://{0}:{1}/{2}", this.ftp_info.ip_addr, this.ftp_info.port, _server_path);
+        string url = string.Format(@"FTP://{0}:{1}/{2}", this._ftpInfo._ipAddress, this._ftpInfo._port, _serverDirPath);
         FtpWebRequest ftpRequest = (FtpWebRequest)WebRequest.Create(url);
-        ftpRequest.Credentials = new NetworkCredential(ftp_info.user, ftp_info.pwd);
+        ftpRequest.Credentials = new NetworkCredential(_ftpInfo._user, _ftpInfo._password);
         ftpRequest.KeepAlive = false;
         ftpRequest.UseBinary = false;
         ftpRequest.UsePassive = false;
@@ -439,34 +371,23 @@ namespace Engine._03.CFTPMgr
       {
         string file_name = open_file_dlg.FileName; //Dialog의 결과이기에 이곳에서만 원본 이미지 파일의 경로를 참조할 수 있다.
         if (false == File.Exists(file_name))
-          return null;//이미지 파일이 없으면 그냥 나감 ㅇㅇ
-                      //return file_name;
+          return null;
 
         var list_file_name = open_file_dlg.FileNames;
-        return (from element in list_file_name
-                select element).ToList<string>();
+        return (from element in list_file_name select element).ToList<string>();
       }
       return null;
     }
-    //6. 파일 다이어로그 출력_저장 위치 선택
-    private string get_save_file_path()
-    {
-      FolderBrowserDialog dialog = new FolderBrowserDialog();
-      dialog.ShowDialog();
-      string select_path = dialog.SelectedPath;    //선택한 다이얼로그 경로 저장
-
-      return select_path;
-    }
 
     //7. 서버의 특정 폴더에 저장된 파일 삭제 
-    private bool delete_saved_file_in_server(string _server_file_path)
+    private bool _DeleteSavedFileInServer(string _serverFilePath)
     {
       bool is_delete = false;
       try
       {
-        string url = string.Format(@"FTP://{0}:{1}/{2}", this.ftp_info.ip_addr, this.ftp_info.port, _server_file_path);
+        string url = string.Format(@"FTP://{0}:{1}/{2}", this._ftpInfo._ipAddress, this._ftpInfo._port, _serverFilePath);
         FtpWebRequest? ftpRequest = (FtpWebRequest)WebRequest.Create(url);
-        ftpRequest.Credentials = new NetworkCredential(ftp_info.user, ftp_info.pwd);
+        ftpRequest.Credentials = new NetworkCredential(_ftpInfo._user, _ftpInfo._password);
         ftpRequest.Method = WebRequestMethods.Ftp.DeleteFile;
 
         using (WebResponse response = ftpRequest.GetResponse())
@@ -482,30 +403,20 @@ namespace Engine._03.CFTPMgr
       }
       return is_delete;
     }
-    //7_2. 서버에서 여러 파일 다운로드
-    private bool delete_saved_file_in_server(List<string> _list_server_file_path)
+    //7_2. 서버의 특정 폴더에 저장된 파일 전체 삭제 
+    private bool _DeleteAllSavedFileInServer(string _serverDirPath)
     {
-      Action<string> action = (string __server_path) => delete_saved_file_in_server(__server_path);
+      Action<string> action = (string __serverDirPath) => _DeleteSavedFileInServer(__serverDirPath);
+      var list_serverFilePath = (from element in GetFileList(_serverDirPath) select element.Insert(0, _serverDirPath)).ToList();
       var list_thread = new List<Thread>();
-      _list_server_file_path.ForEach(element => list_thread.Add(new Thread(() => action(element))));
-      list_thread.ForEach(element => element.Start());
-
-      return true;
-    }
-    //7_3. 서버의 특정 폴더에 저장된 파일 전체 삭제 
-    private bool delete_all_saved_file_in_server(string _server_path)
-    {
-      Action<string> action = (string __server_path) => delete_saved_file_in_server(__server_path);
-      var list_server_file_path = (from element in GetFileList(_server_path) select element.Insert(0, _server_path)).ToList();
-      var list_thread = new List<Thread>();
-      list_server_file_path.ForEach(element => list_thread.Add(new Thread(() => action(element))));
+      list_serverFilePath.ForEach(element => list_thread.Add(new Thread(() => action(element))));
       list_thread.ForEach(element => element.Start());
       return true;
     }
 
     public void Dispose()
     {
-      throw new NotImplementedException();
+
     }
 
 
