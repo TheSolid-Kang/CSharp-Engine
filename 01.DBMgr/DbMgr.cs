990351f5d435d66f85967f6c4a46728ead938bb8
TheSolid-KANG <tortoise8947@gmail.com>
2023-12-21 21:13:56 +0900

    수정사항 1. MSSQL_Mgr: GetDataTable(),GetSPDataTable() 함수 추가 구현   가. GetDataTable(),GetSPDataTable(): 매개변수로 문자열로 url 수집 가능하게 수정 2. DB_Mgr: SelectList() 수정   가. SelectList(): 매개변수로 문자열로 url 수집 가능하게 수정

--- 01.DBMgr/DbMgr.cs
+++ 01.DBMgr/DbMgr.cs
@@ -9,73 +9,77 @@ using System.Threading.Tasks;
 
 namespace Engine._01.DBMgr
 {
-  public class DbMgr
-  {
-    public enum DB_CONNECTION
+    public class DbMgr
     {
-      HOME = 0
-        , ERP
-        , ERP_DEV
-        , MES1
-        , MATERIAL
-        , TWO_MITES
-        , CALEB
-        , GW
-        , END
-    }
-
-
-    public List<T> SelectList<T>(DB_CONNECTION _CON, string _query)
-    {
-      string url = ConfigurationManager.ConnectionStrings[Enum.GetName(_CON)].ConnectionString;
-      List<T> list = null;
-      try
-      {
-        using (SqlConnection conn = new SqlConnection(url))
+        public enum DB_CONNECTION
         {
-
-          conn.Open();
-          SqlCommand cmd = new SqlCommand(_query, conn);
-          SqlDataReader dr = cmd.ExecuteReader();
-          list = DataReaderMapToList<T>(dr);
-          cmd.Dispose();
-          dr.Close();
-
+            HOME = 0
+            , ERP
+            , ERP_DEV
+            , MES1
+            , MATERIAL
+            , TWO_MITES
+            , CALEB
+            , GW
+            , YWDEV
+            , END
         }
-      }
-      catch (Exception _e)
-      {
-        System.Diagnostics.Debug.WriteLine(_e.Message);
-      }
 
-      return list;
-    }
-    private List<T> DataReaderMapToList<T>(IDataReader dr)
-    {
-      List<T> list = new List<T>();
-      try
-      {
-        T obj = default(T);
-        list = new List<T>(dr.FieldCount);
-        while (dr.Read())
+        public List<T> SelectList<T>(string _url, string _query)
         {
-          obj = System.Activator.CreateInstance<T>();
-          foreach (System.Reflection.PropertyInfo prop in obj.GetType().GetProperties())
-          {
-            if (!object.Equals(dr[prop.Name], System.DBNull.Value))
+            List<T> list = null;
+            try
             {
-              prop.SetValue(obj, dr[prop.Name], null);
+                using (SqlConnection conn = new SqlConnection(_url))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(_query, conn);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    list = DataReaderMapToList<T>(dr);
+                    cmd.Dispose();
+                    dr.Close();
+                }
             }
-          }
-          list.Add(obj);
-        }
-      }
-      catch (Exception _e)
-      {
-        System.Diagnostics.Debug.WriteLine(_e.Message);
-      }
-      return list;
-    }
+            catch (Exception _e)
+            {
+                System.Diagnostics.Debug.WriteLine(_e.Message);
+            }
 
-  }
+            return list;
+        }
+
+        public List<T> SelectList<T>(DB_CONNECTION _CON, string _query)
+        {
+            string url = ConfigurationManager.ConnectionStrings[Enum.GetName(_CON)].ConnectionString;
+            return SelectList<T>(url, _query);
+        }
+
+
+        private List<T> DataReaderMapToList<T>(IDataReader dr)
+        {
+            List<T> list = new List<T>();
+            try
+            {
+                T obj = default(T);
+                list = new List<T>(dr.FieldCount);
+                while (dr.Read())
+                {
+                    obj = System.Activator.CreateInstance<T>();
+                    foreach (System.Reflection.PropertyInfo prop in obj.GetType().GetProperties())
+                    {
+                        if (!object.Equals(dr[prop.Name], System.DBNull.Value))
+                        {
+                            prop.SetValue(obj, dr[prop.Name], null);
+                        }
+                    }
+                    list.Add(obj);
+                }
+            }
+            catch (Exception _e)
+            {
+                System.Diagnostics.Debug.WriteLine(_e.Message);
+            }
+            return list;
+        }
+    }
 }
