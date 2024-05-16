bf6cb1a0ea01d675f43deb075845fd4cc579feca
YONWOO\23040302 <caleb0109@yonwookorea.com>
2024-05-16 11:47:02 +0900

    CActiveDirectoryMgr클래스에 GetADUsers 함수 작성

--- 10.CActiveDirectoryMgr/CActiveDirectoryMgr.cs
+++ 10.CActiveDirectoryMgr/CActiveDirectoryMgr.cs
@@ -1,10 +1,13 @@
 ﻿using Engine._98.Headers;
+using Org.BouncyCastle.Bcpg;
+using System.Collections;
 using System.DirectoryServices;
 using System.Reflection.PortableExecutable;
 using DirectoryEntry = System.DirectoryServices.DirectoryEntry;
 
 namespace Engine._10.CActiveDirectoryMgr
 {
+
     public class CActiveDirectoryMgr : GENERIC_MGR<CActiveDirectoryMgr>
     {
         /*
@@ -23,28 +26,73 @@ namespace Engine._10.CActiveDirectoryMgr
             //CN=김동혁(DONGHYEOK KIM),OU=HR계정,OU=Users,OU=_Yonwoo,DC=adtest,DC=com
             //adtest.com/_Yonwoo/Users/HR계정
 
+            //DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://10.225.88.70", "administrator", "yonwoo*211013");
+            DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://10.225.88.70", "administrator", "yonwoo*211013");
+            DirectorySearcher directorySearcher = new DirectorySearcher(directoryEntry)
+            {
+                SearchScope = SearchScope.Subtree
+                ,
+                Filter = $"(&(objectCategory=person)(objectClass=user))"
+            };
+            directorySearcher.PropertiesToLoad.Add("cn");//
+            directorySearcher.PropertiesToLoad.Add("sAMAccountName");//ID를 가져오겠다는 뜻이다.  
+            directorySearcher.PropertiesToLoad.Add("mail"); //mail을 가져오겠다는 뜻이다.
+            directorySearcher.PropertiesToLoad.Add("usergroup"); //usergroup을 가져오겠다는 뜻이다.
+            directorySearcher.PropertiesToLoad.Add("displayname"); //표기이름을 가져오겠다는 뜻이다.
+            SearchResultCollection resultCollection = directorySearcher.FindAll();
+            List<Dictionary<string, string>> ADUsers = new List<Dictionary<string, string>>(resultCollection.Count);
 
-            //DirectoryEntry de = new DirectoryEntry("LDAP://adtest.com");
-            //DirectoryEntry de = new DirectoryEntry("LDAP://adtest.com:3389");
-            //DirectoryEntry de = new DirectoryEntry("LDAP://adtest.com", "administrator", "yonwoo*211013");
-
-            //DirectoryEntry de = new DirectoryEntry("LDAP://CN=Person,CN=Schema,CN=Configuration,DC=adtest,DC=com/Users");
-            //DirectoryEntry de = new DirectoryEntry("LDAP://adtest.com/_Yonwoo/Users/HR계정/");
-
-            DirectoryEntry de = new DirectoryEntry("LDAP://10.225.88.70", "administrator", "yonwoo*211013");
-            //DirectoryEntry de = new DirectoryEntry("LDAP://10.225.88.70:3389");
-            //DirectoryEntry de = new DirectoryEntry("LDAP://10.225.88.70:3389", "administrator", "yonwoo*211013");
-            //DirectoryEntry de = new DirectoryEntry("LDAP://[10.225.88.70]:[3389]");
-            //DirectoryEntry de = new DirectoryEntry("LDAP://[10.225.88.70]:[3389]", "administrator", "yonwoo*211013");
-            //DirectoryEntry de = new DirectoryEntry("LDAP://[10.225.88.70]:[3389]", "administrator", "yonwoo*211013");
-            //DirectoryEntry de = new DirectoryEntry("LDAP://10.225.88.70:3389.com", "administrator", "yonwoo*211013");
-            //DirectoryEntry de = new DirectoryEntry("LDAP://[10.225.88.70]:[3389].com", "administrator", "yonwoo*211013");
-            var temp = de.Children;
-            DirectorySearcher search = new DirectorySearcher(de);
+            foreach (SearchResult searchResult in resultCollection)
+            {
+                Dictionary<string, string> temp = new Dictionary<string, string>();
+                foreach (DictionaryEntry de in searchResult.Properties)
+                    temp.Add(de.Key as string, ((ResultPropertyValueCollection)de.Value)[0].ToString());
+                ADUsers.Add(temp);
+            }
 
 
-            return "LDAP://" + de.Properties["defaultNamingContext"][0].ToString();
+            return "LDAP://" + directoryEntry.Properties["defaultNamingContext"][0].ToString();
         }
+        public List<Users> GetADUsers()
+        {
+            List<Users> lstADUsers = new List<Users>();
+            try
+            {
+                DirectoryEntry searchRoot = new DirectoryEntry("LDAP://10.225.88.70", "administrator", "yonwoo*211013");  //서버도메인 정보, 서버의 계정ID, 계정 PW               
+                DirectorySearcher directorySearcher = new DirectorySearcher(searchRoot);
+                directorySearcher.Filter = $"(&(objectCategory=person)(objectClass=user))";
+                string text = "sAMAccountName"; //ID를 가져오겠다는 뜻이다.                     
+                //directorySearcher.PropertiesToLoad.Add("cn");               
+                directorySearcher.PropertiesToLoad.Add(text);
+                directorySearcher.PropertiesToLoad.Add("mail"); //mail을 가져오겠다는 뜻이다.               
+                directorySearcher.PropertiesToLoad.Add("usergroup"); //usergroup을 가져오겠다는 뜻이다.               
+                directorySearcher.PropertiesToLoad.Add("displayname"); //표기이름을 가져오겠다는 뜻이다.               
+                SearchResultCollection resultCol = directorySearcher.FindAll();
+                SearchResult result; 
+                if (resultCol != null)
+                {
+                    for (int counter = 0; counter < resultCol.Count; counter++)
+                    {
+                        string UserNameEmailString = string.Empty;
+                        result = resultCol[counter];
+                        if (result.Properties.Contains("samaccountname"))
+                        {
+                            Users objSurveyUsers = new Users();
+                            if (result.Properties["samaccountname"] != null)
+                                objSurveyUsers.sAMAccountName = (String)result.Properties["samaccountname"][0];
+                            lstADUsers.Add(objSurveyUsers);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("## Get Search AD User List Exception : " + ex.Message);
+            }
+
+            return lstADUsers;
+        }
+
         void AccessADEntryTest()
         {
             // 사용자의 DirectoryEntry 가져오기
---
+++ 10.CActiveDirectoryMgr/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine._10.CActiveDirectoryMgr
+{
+    public class Users
+    {
+        public string cn { get; set; }
+        public string mail { get; set; }
+        public string sAMAccountName {  get; set; }
+        public string userGroup { get; set; }
+        public string displayName { get; set; }
+    }
+}
