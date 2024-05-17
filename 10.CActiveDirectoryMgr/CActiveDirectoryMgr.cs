7809dac363a6b3c85e05ae913c3039bc6d0191ec
YONWOO\23040302 <caleb0109@yonwookorea.com>
2024-05-17 09:46:49 +0900

    * CActiveDirectoryMgr -> 의미없는 멤버함수 삭제 -> GetADUsers 오버라이딩(외부에서 LDAP URL, ID, PWD 입력 가능하도록 함.) * Users -> searchResult로 확인 할 Properties 추가 //멤버변수 추가

--- 10.CActiveDirectoryMgr/CActiveDirectoryMgr.cs
+++ 10.CActiveDirectoryMgr/CActiveDirectoryMgr.cs
@@ -21,42 +21,13 @@ namespace Engine._10.CActiveDirectoryMgr
             DC      domainComponent
             UID     userid
          */
-        public string GetCurrentDomainPath()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Users> GetADUsers(string? _ldapUrl, string? _username, string? _password)
         {
-            //CN=김동혁(DONGHYEOK KIM),OU=HR계정,OU=Users,OU=_Yonwoo,DC=adtest,DC=com
-            //adtest.com/_Yonwoo/Users/HR계정
-
-            //DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://10.225.88.70", "administrator", "yonwoo*211013");
-            DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://10.225.88.70", "administrator", "yonwoo*211013");
-            DirectorySearcher directorySearcher = new DirectorySearcher(directoryEntry)
-            {
-                SearchScope = SearchScope.Subtree
-                ,
-                Filter = $"(&(objectCategory=person)(objectClass=user))"
-            };
-            directorySearcher.PropertiesToLoad.Add("cn");//
-            directorySearcher.PropertiesToLoad.Add("sAMAccountName");//ID를 가져오겠다는 뜻이다.  
-            directorySearcher.PropertiesToLoad.Add("mail"); //mail을 가져오겠다는 뜻이다.
-            directorySearcher.PropertiesToLoad.Add("usergroup"); //usergroup을 가져오겠다는 뜻이다.
-            directorySearcher.PropertiesToLoad.Add("displayname"); //표기이름을 가져오겠다는 뜻이다.
-            SearchResultCollection resultCollection = directorySearcher.FindAll();
-            List<Dictionary<string, string>> ADUsers = new List<Dictionary<string, string>>(resultCollection.Count);
-
-            foreach (SearchResult searchResult in resultCollection)
-            {
-                Dictionary<string, string> temp = new Dictionary<string, string>();
-                foreach (DictionaryEntry de in searchResult.Properties)
-                    temp.Add(de.Key as string, ((ResultPropertyValueCollection)de.Value)[0].ToString());
-                ADUsers.Add(temp);
-            }
-
-
-            return "LDAP://" + directoryEntry.Properties["defaultNamingContext"][0].ToString();
-        }
-
-        public List<Users> GetADUsers()
-        {
-            DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://10.225.88.70", "administrator", "yonwoo*211013");
+            DirectoryEntry directoryEntry = new DirectoryEntry(_ldapUrl, _username, _password);
             DirectorySearcher directorySearcher = new DirectorySearcher(directoryEntry)
             {
                 SearchScope = SearchScope.Subtree,
@@ -64,6 +35,11 @@ namespace Engine._10.CActiveDirectoryMgr
             };
             return GetADObjs<Users>(directoryEntry, directorySearcher);
         }
+        public List<Users> GetADUsers()
+        {
+            return GetADUsers("LDAP://10.225.88.70", "administrator", "yonwoo*211013");
+        }
+
         /// <summary>
         /// AD 객체(User, Group)
         /// </summary>
@@ -76,10 +52,10 @@ namespace Engine._10.CActiveDirectoryMgr
             try
             {
                 //AD User 가져오기
-                T obj = default(T);
+                T? obj = default(T);
                 obj = System.Activator.CreateInstance<T>();
                 foreach (System.Reflection.PropertyInfo prop in obj.GetType().GetProperties())
-                    _directorySearcher.PropertiesToLoad.Add(prop.Name);
+                    _directorySearcher?.PropertiesToLoad.Add(prop.Name);
             }
             catch (Exception ex)
             {
@@ -93,8 +69,13 @@ namespace Engine._10.CActiveDirectoryMgr
                 T obj = System.Activator.CreateInstance<T>();
                 foreach (System.Reflection.PropertyInfo prop in obj.GetType().GetProperties())
                 {
-                    if(true == searchResult.Properties.Contains(prop.Name))
-                        prop.SetValue(obj, searchResult.Properties[prop.Name][0]);
+                    if (true == searchResult.Properties.Contains(prop.Name))
+                    {
+                        if(prop.Name.Equals("lastLogon") || prop.Name.Equals("lastLogonTimestamp") || prop.Name.Equals("pwdLastSet"))
+                            prop.SetValue(obj, DateTime.FromFileTime((System.Int64)searchResult.Properties[prop.Name][0]));
+                        else
+                            prop.SetValue(obj, searchResult.Properties[prop.Name][0]);
+                    }
                 }
                 list.Add(obj);
             }
@@ -102,39 +83,5 @@ namespace Engine._10.CActiveDirectoryMgr
             return list;
         }
 
-        void AccessADEntryTest()
-        {
-            // 사용자의 DirectoryEntry 가져오기
-            DirectoryEntry userEntry = new DirectoryEntry("LDAP://CN=John Doe,CN=Users,DC=domain,DC=com");
-
-            // Active Directory에서 사용자 찾기
-            DirectorySearcher searcher = new DirectorySearcher(userEntry);
-            searcher.Filter = "(samaccountname=johndoe)";
-            SearchResult result = searcher.FindOne();
-
-            if (result != null)
-            {
-                DirectoryEntry foundUser = result.GetDirectoryEntry();
-                // 사용자 속성에 액세스
-                string username = foundUser.Properties["samaccountname"].Value.ToString();
-            }
-        }
-        void AccessADEntry(string _url, string _ldapId, string _ldapPwd, string _domain, string _userId, string _userPwd)
-        {
-            // 사용자의 DirectoryEntry 가져오기
-            DirectoryEntry userEntry = new DirectoryEntry("LDAP://CN=John Doe,CN=Users,DC=domain,DC=com");
-
-            // Active Directory에서 사용자 찾기
-            DirectorySearcher searcher = new DirectorySearcher(userEntry);
-            searcher.Filter = "(samaccountname=johndoe)";
-            SearchResult result = searcher.FindOne();
-
-            if (result != null)
-            {
-                DirectoryEntry foundUser = result.GetDirectoryEntry();
-                // 사용자 속성에 액세스
-                string username = foundUser.Properties["samaccountname"].Value.ToString();
-            }
-        }
     }
 }
--- 10.CActiveDirectoryMgr/Users.cs
+++ 10.CActiveDirectoryMgr/Users.cs
@@ -8,10 +8,23 @@ namespace Engine._10.CActiveDirectoryMgr
 {
     public class Users
     {
-        public string cn { get; set; }
-        public string mail { get; set; }
-        public string sAMAccountName {  get; set; }
-        public string userGroup { get; set; }
-        public string displayName { get; set; }
+        public string cn { get; set; } = "";
+        public string department { get; set; } = "";
+        public string displayName { get; set; } = "";
+        public string distinguishedName { get; set; } = "";
+        public string givenName { get; set; } = ""; //이름
+        public DateTime lastLogon { get; set; }
+        public DateTime lastLogonTimestamp { get; set; }
+        public string name { get; set; } = ""; //이름 전체
+        public int primaryGroupID { get; set; }
+        public DateTime pwdLastSet { get; set; }
+        public string sn { get; set; } = ""; //성
+        public string title { get; set; } = ""; //직급
+        public string mail { get; set; } = "";
+        public string sAMAccountName { get; set; } = "";
+        public string userGroup { get; set; } = "";
+        public string userPrincipalName { get; set; } = ""; //yonwoo.cos / yonwookorea.com 구분
+        public DateTime whenChanged { get; set; }
+        public DateTime whenCreated { get; set; }
     }
 }
