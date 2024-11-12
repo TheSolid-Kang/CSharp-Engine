e9f31a2030a75838b93d71f94ebcb64dee77144b
TheSolid-KANG <tortoise8947@gmail.com>
2024-11-12 13:29:12 +0900

    파일 관리 메서드 수정: - GetFileNames(): 파일 선택 대화 상자를 통해 선택된 파일들의 이름을 반환하도록 구현 - GetFileNames(string _rootPath): 지정된 디렉토리 내의 모든 파일 이름을 반환하도록 구현 - GetRecursiveFileNames(string _rootDirPath): 지정된 디렉토리와 하위 디렉토리에서 파일이 포함된 디렉토리의 경로를 반

--- 08.CFileMgr/CFileMgr.cs
+++ 08.CFileMgr/CFileMgr.cs
@@ -12,30 +12,60 @@ namespace Engine._08.CFileMgr
 {
     public class CFileMgr : GENERIC_MGR<CFileMgr>
     {
+        /// <summary>
+        /// 파일 선택 대화 상자를 통해 사용자가 선택한 파일들의 이름을 반환합니다.
+        /// 
+        /// HOW:
+        /// 1. 파일 필터를 설정합니다.
+        /// 2. OpenFileDialog를 사용하여 파일 선택 대화 상자를 표시합니다.
+        /// 3. 사용자가 파일을 선택하면, 선택된 파일들의 이름을 List<string> 형태로 반환합니다.
+        /// 4. 파일이 선택되지 않았거나 존재하지 않으면 null을 반환합니다.
+        /// </summary>
+        /// <returns>선택된 파일들의 이름을 담은 List<string> 객체 또는 null</returns>
         public List<string>? GetFileNames()
         {
-            System.Text.StringBuilder string_builder_filter = new System.Text.StringBuilder(1024);
-            string_builder_filter.Append("All Files(*.txt; *.md; *.jpg; *.gif; *.bmp; *.png)|*.txt;*.md;*.jpg;*.jpeg;*.gif;*.bmp;*.png");
-            string_builder_filter.Append("|BMP 파일(*.bmp)|*.bmp|Jpg 파일(*.jpg)|*.jpg|PNG 파일(*.png)|*.png");
-            string_builder_filter.Append("|GIF 파일(*.gif)|*.gif");
-            string_builder_filter.Append("|txt 파일(*.txt)|*.txt");
+            // 파일 필터 설정
+            var filter = new StringBuilder(1024)
+                .Append("All Files(*.txt; *.md; *.jpg; *.gif; *.bmp; *.png)|*.txt;*.md;*.jpg;*.jpeg;*.gif;*.bmp;*.png")
+                .Append("|BMP 파일(*.bmp)|*.bmp|Jpg 파일(*.jpg)|*.jpg|PNG 파일(*.png)|*.png")
+                .Append("|GIF 파일(*.gif)|*.gif")
+                .Append("|txt 파일(*.txt)|*.txt")
+                .ToString();
 
-            System.Windows.Forms.OpenFileDialog open_file_dlg = new System.Windows.Forms.OpenFileDialog();
-            open_file_dlg.Filter = string_builder_filter.ToString();
-            //open_file_dlg.InitialDirectory = System.Environment.CurrentDirectory;         //초기경로
-            open_file_dlg.RestoreDirectory = true;                 //현재 경로가 이전 경로로 복원되는지 여부          
-            open_file_dlg.Multiselect = true;                      //여러파일선택
-            open_file_dlg.Title = "등록할 파일을 선택하세요.";
-            if (System.Windows.Forms.DialogResult.OK == open_file_dlg.ShowDialog())
+            // OpenFileDialog 설정
+            var openFileDialog = new System.Windows.Forms.OpenFileDialog
             {
-                if (false == File.Exists(open_file_dlg.FileNames[0]))
-                    return null; //파일이 없으면 그냥 나감 
+                Filter = filter,
+                RestoreDirectory = true, // 현재 경로가 이전 경로로 복원되는지 여부
+                Multiselect = true,      // 여러 파일 선택 가능
+                Title = "등록할 파일을 선택하세요."
+            };
 
-                var list_file_name = open_file_dlg.FileNames;
-                return (from element in list_file_name select element).ToList<string>();
+            // 파일 대화 상자를 표시하고 사용자가 파일을 선택했는지 확인
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // 선택된 파일이 존재하는지 확인
+                if (!File.Exists(openFileDialog.FileNames[0]))
+                    return null; // 파일이 없으면 null 반환
+
+                // 선택된 파일들의 이름을 리스트로 반환
+                return openFileDialog.FileNames.ToList();
             }
-            return null;
+
+            return null; // 파일을 선택하지 않았으면 null 반환
         }
+
+        /// <summary>
+        /// 지정된 디렉토리 내의 모든 파일 이름을 반환합니다.
+        /// 
+        /// HOW:
+        /// 1. 지정된 경로의 DirectoryInfo 객체를 생성합니다.
+        /// 2. DirectoryInfo 객체를 사용하여 디렉토리 내의 모든 파일 정보를 가져옵니다.
+        /// 3. 각 파일의 전체 경로를 List<string>에 추가합니다.
+        /// 4. 파일 이름 목록을 반환합니다.
+        /// </summary>
+        /// <param name="_rootPath">파일 이름을 조회할 디렉토리의 경로</param>
+        /// <returns>디렉토리 내의 모든 파일 이름을 담은 List<string> 객체 또는 null</returns>
         public List<string>? GetFileNames(string _rootPath)
         {
             var directoryInfo = new DirectoryInfo(_rootPath);
@@ -44,19 +74,36 @@ namespace Engine._08.CFileMgr
             list.ForEach(file => { fileNames.Add(file.FullName); });
             return fileNames;
         }
+
+        /// <summary>
+        /// 지정된 디렉토리와 하위 디렉토리에서 파일이 포함된 디렉토리의 경로를 반환합니다.
+        /// 
+        /// HOW:
+        /// 1. 지정된 경로의 모든 하위 디렉토리를 열거합니다.
+        /// 2. 각 디렉토리 내에 파일이 있는지 확인합니다.
+        /// 3. 파일이 있는 디렉토리의 경로를 리스트에 추가합니다.
+        /// 4. 파일이 포함된 디렉토리 경로 목록을 반환합니다.
+        /// </summary>
+        /// <param name="_rootDirPath">디렉토리 경로</param>
+        /// <returns>파일이 포함된 디렉토리 경로를 담은 List<string> 객체 또는 null</returns>
         public List<string>? GetRecursiveFileNames(string _rootDirPath)
         {
             var directories = Directory.EnumerateDirectories(_rootDirPath, "*.*", SearchOption.AllDirectories).ToList();
             List<string> dirPaths = new List<string>();
-            foreach (var _dir in directories)
+            foreach (var dir in directories)
             {
-                DirectoryInfo _dirInfo = new DirectoryInfo(_dir);
-                var files = _dirInfo.GetFiles().ToList();
-                if (files.Count != 0)
-                    dirPaths.Add(_dir); //파일을 가지고 있는 디렉토리만 추가
+                DirectoryInfo dirInfo = new DirectoryInfo(dir);
+                if (dirInfo.GetFiles().Any())
+                {
+                    dirPaths.Add(dir); // 파일이 있는 디렉토리만 추가
+                }
             }
             return dirPaths;
         }
+
+
+
+        
 
         #region txt, md 파일 관련
         public IEnumerable<string> GetFileTextIter(string _path)
