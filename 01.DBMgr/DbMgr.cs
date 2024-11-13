076f143e3b2fe6f3806e1f30a71eb0f49de0c2ec
TheSolid-KANG <tortoise8947@gmail.com>
2024-11-13 11:17:00 +0900

    DbMgr 클래스 수정: 1. 클래스 내 모든 메서드 WHAT, HOW 주석처리 2. ConvertListToDataTable<T>(List<T>? items) 메서드 오류수정   가. 오류: NotSupportedException: 'DataSet does not support System.Nullable<>

--- 01.DBMgr/DbMgr.cs
+++ 01.DBMgr/DbMgr.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.DirectoryServices.ActiveDirectory;
 
 namespace Engine._01.DBMgr
 {
@@ -28,6 +29,17 @@ namespace Engine._01.DBMgr
             , END
         }
 
+        /// <summary>
+        /// WHAT: 지정된 URL과 쿼리를 사용하여 데이터베이스에서 데이터를 조회하고, 결과를 List<T> 형태로 반환합니다.
+        /// HOW: 
+        /// 1. SqlConnection을 사용하여 데이터베이스에 연결합니다.
+        /// 2. SqlCommand를 사용하여 쿼리를 실행하고 SqlDataReader로 결과를 읽습니다.
+        /// 3. DataReaderMapToList<T> 메서드를 호출하여 결과를 List<T>로 변환합니다.
+        /// </summary>
+        /// <typeparam name="T">반환할 객체의 타입</typeparam>
+        /// <param name="_url">데이터베이스 연결 문자열</param>
+        /// <param name="_query">실행할 SQL 쿼리</param>
+        /// <returns>조회된 데이터를 담은 List<T> 객체</returns>
         public List<T> SelectList<T>(string _url, string _query)
         {
             List<T> list = null;
@@ -51,13 +63,32 @@ namespace Engine._01.DBMgr
             return list;
         }
 
+        /// <summary>
+        /// WHAT: 지정된 DB_CONNECTION과 쿼리를 사용하여 데이터베이스에서 데이터를 조회하고, 결과를 List<T> 형태로 반환합니다.
+        /// HOW: 
+        /// 1. ConfigurationManager를 사용하여 연결 문자열을 가져옵니다.
+        /// 2. SelectList<T> 메서드를 호출하여 데이터를 조회합니다.
+        /// </summary>
+        /// <typeparam name="T">반환할 객체의 타입</typeparam>
+        /// <param name="_CON">DB_CONNECTION 열거형 값</param>
+        /// <param name="_query">실행할 SQL 쿼리</param>
+        /// <returns>조회된 데이터를 담은 List<T> 객체</returns>
         public List<T> SelectList<T>(DB_CONNECTION _CON, string _query)
         {
             string url = ConfigurationManager.ConnectionStrings[Enum.GetName(_CON)].ConnectionString;
             return SelectList<T>(url, _query);
         }
 
-
+        /// <summary>
+        /// WHAT: IDataReader를 사용하여 데이터를 읽고, 결과를 List<T> 형태로 반환합니다.
+        /// HOW: 
+        /// 1. IDataReader를 사용하여 데이터를 읽습니다.
+        /// 2. 각 행을 객체로 변환합니다.
+        /// 3. 변환된 객체를 리스트에 추가합니다.
+        /// </summary>
+        /// <typeparam name="T">반환할 객체의 타입</typeparam>
+        /// <param name="dr">IDataReader 객체</param>
+        /// <returns>조회된 데이터를 담은 List<T> 객체</returns>
         private List<T> DataReaderMapToList<T>(IDataReader dr)
         {
             List<T> list = new List<T>();
@@ -84,13 +115,30 @@ namespace Engine._01.DBMgr
             }
             return list;
         }
+
+        /// <summary>
+        /// WHAT: List<T>를 DataTable로 변환합니다.
+        /// HOW: 
+        /// 1. DataTable 객체를 생성하고 컬럼을 추가합니다.
+        /// 2. List<T>의 각 항목을 DataRow로 변환하여 DataTable에 추가합니다.
+        /// </summary>
+        /// <typeparam name="T">리스트의 객체 타입</typeparam>
+        /// <param name="items">변환할 List<T> 객체</param>
+        /// <returns>변환된 DataTable 객체</returns>
         public DataTable ConvertListToDataTable<T>(List<T>? items)
         {
             var tb = new DataTable(typeof(T).Name);
             PropertyInfo[] props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
             foreach (var prop in props)
-                tb.Columns.Add(prop.Name, prop.PropertyType);
+            {
+                var propType = prop.PropertyType;
+                if (propType.IsGenericType && propType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                {
+                    propType = Nullable.GetUnderlyingType(propType);
+                }
+                tb.Columns.Add(prop.Name, propType);
+            }
             foreach (var item in items)
             {
                 var values = new object[props.Length];
@@ -101,6 +149,16 @@ namespace Engine._01.DBMgr
             return tb;
         }
 
+        /// <summary>
+        /// WHAT: DataTable을 List<T>로 변환합니다.
+        /// HOW: 
+        /// 1. DataTable의 각 행을 읽습니다.
+        /// 2. 각 행을 객체로 변환합니다.
+        /// 3. 변환된 객체를 리스트에 추가합니다.
+        /// </summary>
+        /// <typeparam name="T">리스트의 객체 타입</typeparam>
+        /// <param name="_dataTable">변환할 DataTable 객체</param>
+        /// <returns>변환된 List<T> 객체</returns>
         public List<T> ConvertDataTableToList<T>(DataTable _dataTable)
         {
             List<T> list = new List<T>(_dataTable.Rows.Count);
@@ -108,7 +166,6 @@ namespace Engine._01.DBMgr
 
             try
             {
-
                 foreach (DataRow dataRow in _dataTable.Rows)
                 {
                     T obj = System.Activator.CreateInstance<T>();
@@ -136,11 +193,26 @@ namespace Engine._01.DBMgr
     {
         private string _connectionString;
 
+        /// <summary>
+        /// WHAT: SqlProcedureExecutor 생성자
+        /// HOW: 
+        /// 1. 생성자 매개변수로 전달된 연결 문자열을 _connectionString 필드에 저장합니다.
+        /// </summary>
+        /// <param name="connectionString">데이터베이스 연결 문자열</param>
         public SqlProcedureExecutor(string connectionString)
         {
             _connectionString = connectionString;
         }
 
+        /// <summary>
+        /// WHAT: 지정된 프로시저 이름과 파라미터를 사용하여 SQL 프로시저를 실행합니다.
+        /// HOW: 
+        /// 1. SqlConnection을 사용하여 데이터베이스에 연결합니다.
+        /// 2. SqlCommand를 사용하여 프로시저를 설정하고 파라미터를 추가합니다.
+        /// 3. ExecuteNonQuery를 호출하여 프로시저를 실행합니다.
+        /// </summary>
+        /// <param name="procedureName">실행할 프로시저 이름</param>
+        /// <param name="parameters">프로시저에 전달할 파라미터 배열</param>
         public void ExecuteProcedure(string procedureName, SqlParameter[] parameters)
         {
             using (SqlConnection connection = new SqlConnection(_connectionString))
@@ -169,29 +241,4 @@ namespace Engine._01.DBMgr
             }
         }
     }
-
-    /*
-     class Program
-{
-    static void Main(string[] args)
-    {
-        string connectionString = "Server=your_server;Database=your_database;User Id=your_username;Password=your_password;"; // 연결 문자열을 입력하세요.
-        SqlProcedureExecutor executor = new SqlProcedureExecutor(connectionString);
-
-        string procedureName = "YourStoredProcedure"; // 실행할 프로시저 이름
-        SqlParameter[] parameters = new SqlParameter[]
-        {
-            new SqlParameter("@Parameter1", SqlDbType.Int) { Value = 10 },
-            new SqlParameter("@Parameter2", SqlDbType.VarChar) { Value = "Sample" }
-        };
-
-        executor.ExecuteProcedure(procedureName, parameters);
-    }
-}
-
-    SqlProcedureExecutor 클래스: 이 클래스는 SQL Server에 연결하고 프로시저를 실행하는 기능을 제공합니다.
-ExecuteProcedure 메서드: 주어진 프로시저 이름과 파라미터 배열을 사용하여 프로시저를 실행합니다.
-사용 예제: 연결 문자열, 프로시저 이름, 및 파라미터를 설정하고 ExecuteProcedure 메서드를 호출하여 프로시저를 실행합니다.
-이 코드를 사용하여 SQL 프로시저를 쉽게 호출할 수 있습니다. 필요에 따라 예외 처리를 추가하거나, 반환 값을 처리하는 기능을 추가할 수 있습니다.
-     */
 }
